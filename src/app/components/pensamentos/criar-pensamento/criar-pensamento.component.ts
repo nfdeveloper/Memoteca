d50cb6c93e39401915af0cc04eddfd5ff767700e
nfdeveloper <nfdeveloper04@gmail.com>
2023-09-11 22:54:02 -0300

    Adicionando lógica para desabilitar botão

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -44,4 +44,13 @@ export class CriarPensamentoComponent implements OnInit {
   cancelar(){
     this.router.navigate(['/listarPensamento'])
   }
+
+  habilitarBotao(): string{
+    if(this.formulario.valid){
+      return 'botao'
+    }
+    else{
+      return 'botao__desabilitado'
+    }
+  }
 }
